0c2de742974add499fb4262565a4549231c50620
trader2026star <mhmdbrkat828@gmail.com>
2026-09-24 18:01:59 +0300

    Remove debug flag from app.run()

--- main.py
+++ main.py
@@ -72,4 +72,4 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
